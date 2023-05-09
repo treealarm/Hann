8987fd14cab5485b12550d498f387319c4df1bbc
danil.serov <serovdanil@gmail.com>
2023-05-09 14:49:14 +0700

    chore

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { AppBar, Box, Card, CardContent, Grid, Paper, Toolbar, Typography } from '@mui/material';
+import { TreeControl } from './TreeControl.tsx';
+
+function App() {
+  return (
+    <div className="App">
+<Box sx={{ height: '98vh' }}>
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: '#bbbbbb' }} >
+      <Toolbar variant='dense' />
+      </AppBar>
+      <Toolbar variant='dense' />
+    </Box>
+      <Grid container sx={{ height: 'calc(100% - 60px)' }}>
+      
+        <Grid item xs={3} sx={{ height: "100%"}}>
+            <Paper sx={{ height: "100%", width: "100%" }} >
+              <TreeControl/>
+            </Paper>
+
+          </Grid>
+
+          <Grid item xs sx={{ minWidth: "100px" }}>
+
+            <Box sx={{ height: '100%' }}>
+              
+            </Box>
+
+          </Grid>
+
+        <Grid item xs={3} sx={{ height: "100%"}}>
+          <Paper sx={{ height: "100%", overflow: 'auto', width: "100%" }}>
+            
+            </Paper>
+
+          </Grid>
+        </Grid>
+    </Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TreeControl.tsx
@@ -0,0 +1,86 @@
+﻿import * as React from 'react';
+
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import { Box, Toolbar } from '@mui/material';
+
+
+
+export function TreeControl() {
+  
+  interface MyObject {
+    id: string;
+    name: string;
+  }
+
+  var markers:MyObject[] = [];
+
+  for (var i = 0; i < 100; i++)
+  {
+markers.push(
+  {
+    id:i.toString(),
+    name:i.toString()
+  });
+  }
+
+    return (
+      <Box sx={{
+            width: '100%',            
+            height: '100%'
+            }}>
+        <Box sx={{ flexGrow: 1, backgroundColor: 'lightgray' }}>
+          <Toolbar variant="dense">
+            <IconButton edge="start">
+              
+            </IconButton>
+            <Box sx={{ flexGrow: 1 }} />
+            <IconButton edge="end">
+              
+            </IconButton>
+          </Toolbar>
+        </Box> 
+
+        <Box sx={{
+            width: '100%',            
+            height: 'calc(100% - 60px)',
+            overflow: 'auto'
+            }}>
+        <List dense sx={{width: "100%", backgroundColor: 'lightgreen', height:"100%"  }}>
+         {
+          markers?.map((marker, index) =>
+            <ListItem
+              key={marker.id}
+              disablePadding
+             >
+              <ListItemButton
+                >
+                <ListItemIcon>
+                  <Checkbox
+                    size="small"
+                    edge="start"
+                  
+                    tabIndex={-1}
+                    disableRipple
+                    id={marker.id}
+                  
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  id={marker.id}
+                  primary={marker.name}                  
+                />
+              </ListItemButton>
+          </ListItem>
+        )}
+          </List>
+          </Box>
+        </Box>
+    );
+}
